97787cb461538f1e8172231d1c85936ede96dcaf
nicor528 <nicolasriquelme528@gmail.com>
2025-11-12 20:00:10 -0300

    after clase 12

--- ejercicio-clase12/src/models/firebase.js
+++ ejercicio-clase12/src/models/firebase.js
@@ -1,27 +1,105 @@
 import { db } from "../data/data.js";
-import { doc, getDoc, collection, getDocs } from "firebase/firestore";
+import { doc, getDoc, collection, getDocs, setDoc, addDoc, updateDoc, deleteDoc, } from "firebase/firestore";
 
-async function obtenerProducto(){
-  const docRef = doc(db, "products", "DElBF7JlDalysKPKkIDW");
-  const docSnap = await getDoc(docRef);
+function obtenerProducto(id){
+  return new Promise(async (res, rej) => {
+    try{
+      const docRef = doc(db, "products", id);
+      const docSnap = await getDoc(docRef);
 
-  if (docSnap.exists()) {
-    console.log("Snap data: ", docSnap)
-    console.log("Document ID:", docSnap.id);
-    console.log("Document data:", docSnap.data());
-  } else {
-    // docSnap.data() will be undefined in this case
-    console.log("No such document!");
-  }
+      if (docSnap.exists()) {
+        console.log("Snap data: ", docSnap)
+        console.log("Document ID:", docSnap.id);
+        console.log("Document data:", docSnap.data());
+        res(docSnap.data())
+      } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }catch(error){
+      console.log(error)
+      rej(error)
+    }
+  })
+  
 }
-obtenerProducto()
+//obtenerProducto(id)
 
-async function obtenerProductos(){
-  const querySnapshot = await getDocs(collection(db, "products"));
-  console.log("Snap completa: ", querySnapshot)
-  querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
-  });
+function obtenerProductos(){
+  return(
+    new Promise(async (res, rej) => {
+      try{
+        const querySnapshot = await getDocs(collection(db, "products"));
+        console.log("Snap completa: ", querySnapshot)
+        const productos = []
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+          productos.push({...doc.data(), id: doc.id}) 
+        });
+        console.log(productos)
+        res(productos)
+      }catch(error){
+        console.log(error)
+        rej(error)
+      }
+    })
+  )
 }
 obtenerProductos()
+
+function agregarProducto(producto){
+  return(
+    new Promise(async (res, rej) => {
+        try{
+          const docRef = await addDoc(collection(db, "products"), producto);
+          console.log("Doc ID: ", docRef.id, "Producto: ", docRef)
+          res({...producto, id: docRef.id})
+        }catch(error){
+          console.log(error)
+          rej(error)
+        }
+    })
+  )
+
+}
+
+//agregarProducto({nombre: "yerba", categoria: "infusion", precio: 200})
+
+function actualizarProducto(producto){
+  return(
+    new Promise(async (res, rej) => {
+      try{
+        await updateDoc(doc(db, "products", producto.id), {
+          precio: producto.precio
+        })
+        console.log("producto actualizado")
+        res()
+      }catch(error){
+        console.log(error)
+        rej(error)
+      }
+    })
+  )
+
+}
+
+//actualizarProducto({id: "6VOeGkGEHLxBrdU3QVzV", precio: 220})
+
+function eliminarProducto(id){
+  return(
+    new Promise(async (res, rej) => {
+      try{
+        await deleteDoc(doc(db, "products", id));
+        console.log("Producto eliminado")
+        res()
+      }catch(error){
+        console.log(error)
+        rej(error)
+      }
+    })
+  )
+
+}
+
+//eliminarProducto("KLFLJoCvvatJTUH5Vx3B")
